cdc112ee877fc0411e4e1fbd569b93ddedbb1590
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2016-08-22 08:33:36 +0000

    commit
    git-svn-id: file:///e/Dropbox/SVN/scripts-repo/utilities/pkrf@189 338c1eee-db2b-b046-b223-49336138bb35

--- plot-raster-over-rdp.r
+++ plot-raster-over-rdp.r
@@ -3,12 +3,12 @@
 
 #' Raster plot over RDP
 #'
-#' Function to plot rasters correctly over an remote desktop protocol connection
+#' Function to plot rasters correctly over an Windows remote desktop protocol connection
 #' 
 #' @param x Raster as defined by the \code{"raster"} package.
 #' @param ... Other parameters parsed to the raster plot function.
 #'
-#' @return None
+#' @return Plot of the raster that is correctly displayed
 #' @export
 rrplot <- function(x,...){
   plot(x,useRaster=F,...)
