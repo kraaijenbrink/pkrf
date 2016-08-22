834342892e265eaeddec7737e38e6388bfc59609
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2016-08-22 08:27:43 +0000

    added roxygen
    git-svn-id: file:///e/Dropbox/SVN/scripts-repo/utilities/pkrf@187 338c1eee-db2b-b046-b223-49336138bb35

--- plot-raster-over-rdp.r
+++ plot-raster-over-rdp.r
@@ -1,3 +1,17 @@
 # function to plot rasters over RDP connection
 # 'Remote Raster Plot'
-rrplot <- function(x,...){plot(x,useRaster=F,...)}
+
+#' Raster plot over RDP
+#'
+#' Function to plot rasters correctly over an remote desktop protocol connection
+#' 
+#' @param x Raster as defined by the \code{"raster"} package.
+#' @param ... Other parameters parsed to the raster plot function.
+#'
+#' @return None
+#' @export
+#'
+#' @examples
+rrplot <- function(x,...){
+  plot(x,useRaster=F,...)
+}
