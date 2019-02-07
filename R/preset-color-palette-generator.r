6a19c7d595c9efbefa8717e8da6c15d25813fb60
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2019-02-07 13:27:16 +0100

    added some palettes from: https://www.color-hex.com/color-palettes/popular.php

--- R/preset-color-palette-generator.r
+++ R/preset-color-palette-generator.r
@@ -14,7 +14,7 @@
 #' @export
 pkPal <- function(name='Set1', number=0, reversed=F, random=F){
   
-  namelist <- c('Set1','Set2','Set3','Pastel1','Pastel2','Accent','Dark')
+  namelist <- c('Set1','Set2','Set3','Pastel1','Pastel2','Accent','Dark', 'Google','Raven','Muted','Cappuccino')
   if (sum(namelist %in% name)){
     message <- paste0('Color palette not available. Choose any of:\n',paste(sort(namelist),collapse=', '))
   }
@@ -25,7 +25,11 @@ pkPal <- function(name='Set1', number=0, reversed=F, random=F){
                      c("FBB4AE","B3CDE3","CCEBC5","DECBE4","FED9A6","FFFFCC","E5D8BD","FDDAEC","F2F2F2"),
                      c("B3E2CD","FDCDAC","CBD5E8","F4CAE4","E6F5C9","FFF2AE","F1E2CC","CCCCCC"),
                      c("7FC97F","BEAED4","FDC086","FFFF99","386CB0","F0027F","BF5B17","666666"),
-                     c("1B9E77","D95F02","7570B3","E7298A","66A61E","E6AB02","A6761D","666666")
+                     c("1B9E77","D95F02","7570B3","E7298A","66A61E","E6AB02","A6761D","666666"),
+                     c('008744','0057e7','d62d20','ffa700','000000'),
+                     c('0e1a40','222f5b','5d5d5d','946b2d','000000'),
+                     c('2e4045','83adb5','c7bbc9','5e3c58','bfb5b2'),
+                     c('4b3832','854442','E5DBCF','3c2f2f','be9b7b')
   )
   
   if (reversed){colpal <- rev(colpal)}
