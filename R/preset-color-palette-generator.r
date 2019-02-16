09b750bfb49a6c48c40ec5769498e28813a4a53c
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2019-02-16 17:00:49 +0100

    added show-all functionality to color ramp/palette functions

--- R/preset-color-palette-generator.r
+++ R/preset-color-palette-generator.r
@@ -9,18 +9,21 @@
 #' @param number Number of output colors desired (integer). 
 #' @param reversed Reverse palette order (logical).
 #' @param reversed Randomize palette order (logical).
-#'
+#' @param show Show a plot of all available color palettes (logical).
+#' 
 #' @return Vector with hex colors strings. 
 #' @export
-pkPal <- function(name='Set1', number=0, reversed=F, random=F){
+pkPal <- function(name='Set1', number=0, reversed=F, random=F, show=F){
   
   namelist <- c('Set1','Set2','Set3','Pastel1','Pastel2','Accent','Dark', 'Google','Raven','Muted','Cappuccino',
                 'Pale1','Water','Skin','NS','Legendary','WarmWall', 'Oasis','Earthly','A3','AfterCold','Swoop',
-                'BurbleTeal')
+                'BurbleTeal','UU')
   if (sum(tolower(namelist) %in% tolower(name))==0){
     message <- stop(paste0('Color palette not available. Choose any of:\n',paste(sort(namelist),collapse='\n')))
   }
-  colpal   <- switch(which(tolower(namelist) %in% tolower(name)),
+  
+  getPal <- function(name){
+    switch(which(tolower(namelist) %in% tolower(name)),
                      c("E41A1C","377EB8","4DAF4A","984EA3","FF7F00","FFFF33","A65628","F781BF","999999"),
                      c("66C2A5","FC8D62","8DA0CB","E78AC3","A6D854","FFD92F","E5C494","B3B3B3"),
                      c("8DD3C7","FFFFB3","BEBADA","FB8072","80B1D3","FDB462","B3DE69","FCCDE5","D9D9D9","BC80BD","CCEBC5"),
@@ -43,22 +46,46 @@ pkPal <- function(name='Set1', number=0, reversed=F, random=F){
                      c('1c3144','d00000','ffba08','a2aebb','3f88c5'),
                      c('171219','225560','edf060','f0803c','310d20'),
                      c('dd7373','3b3561','ead94c','d1d1d1','51a3a3'),
-                     c('2d3047','93b7be','e0ca3c','a799b7','048a81')
-                     
-                     
-                     
-  )
-  
-  if (reversed){colpal <- rev(colpal)}
-  if (random){colpal   <- sample(colpal)}
-  if (number>length(colpal)){
-    outcolors <- paste0('#',colpal)
-    warning(paste0('The ',name,' palette has only ',length(colpal), ' colors. Returning vector with only those.'))
-  }else if (number==0){
-    outcolors <- paste0('#',colpal)
-  }else{
-    outcolors <- paste0('#',colpal)[1:number]
+                     c('2d3047','93b7be','e0ca3c','a799b7','048a81'),
+                     c('FFCD00','C00A35','000000','FFE6AB','F3965E','AA1555','6E3B23','24A793','5287C6','001240','5B2182')
+    )
   }
-  return(outcolors)
+  
+  
+  if (show){ # make plot of available ramps
+    
+    spac <- 0.33
+    all <- lapply(namelist, function(x) (paste0('#',getPal(x))))
+    windows(600,900, bg='#2d2d2d')
+    op  <- par(mar=c(0.5,8,0.5,1))
+    plot(NA,axes=F,xlab='',ylab='',ylim=c(0,length(namelist)+1), xlim=c(0,1), xaxs='i', yaxs='i')
+    for (i in 1:length(namelist)){
+      ncolor <- length(all[[i]])
+      delta <- 1/ncolor
+      for (j in 1:ncolor){
+        p <- j/ncolor
+        irev <-  (length(namelist):1)[i]
+        polygon(x=c(p-delta,p,p,p-delta), y=c(irev-spac,irev-spac,irev+spac,irev+spac), border=NA, lwd=0.5, col=all[[i]][j])
+      }
+    }
+    mtext(namelist, side=2, line=0.5, at=length(namelist):1,padj=0.5,adj=1,las=2, col='white', font=1, cex=1.1)
+    par(op)
+    warning('No color palette output when "show=TRUE", just a plot with available color palettes')
+  
+  
+  }else{
+    colpal <- getPal(name)
+    if (reversed){colpal <- rev(colpal)}
+    if (random){colpal   <- sample(colpal)}
+    if (number>length(colpal)){
+      outcolors <- paste0('#',colpal)
+      warning(paste0('The ',name,' palette has only ',length(colpal), ' colors. Returning vector with only those.'))
+    }else if (number==0){
+      outcolors <- paste0('#',colpal)
+    }else{
+      outcolors <- paste0('#',colpal)[1:number]
+    }
+    return(outcolors)
+  }
 }
 
--- R/preset-color-ramp-generator.r
+++ R/preset-color-ramp-generator.r
@@ -8,41 +8,65 @@
 #' @param name Name of the color ramp (string).
 #' @param number Number of output colors desired (integer).
 #' @param reversed Should the ramp be reversed (logical).
-#'
+#' @param show Show a plot of all available color ramps (logical).
+#' 
 #' @return Vector with hex colors strings. 
 #' @export
 
-pkRamp <- function(name='parula', number=100, reversed=F){
+pkRamp <- function(name='parula', number=100, reversed=F, show=F){
   
   namelist <- c('Jet','Parula','Viridis','Inferno','Magma','Plasma','Cividis','ElevAG','WtSpec','RwB','PwG','OwP','Spectral',
                 'ElevNat1','ElevNat2','RwBsoft','RwBpale','Taupe')
   if (sum(tolower(namelist) %in% tolower(name))==0){
     message <- stop(paste0('Color ramp not available. Choose any of:\n',paste(sort(namelist),collapse='\n')))
   }
-  colpal   <- switch(which(tolower(namelist) %in% tolower(name)),
-    c("0000FF","0080FF","00FFFF","80FF80","FFFF00","FF8000","FF0000","800000"),
-    c("352A87","0567DF","108ED0","1BAEB4","6BBC85","C4BB5E","F7CB33","F9FB0E"),
-    c("440154","482878","3E4A89","31688E","26828E","1F9E89","35B779","6DCD59","B4DE2C","FDE725"),
-    c("000004","1B0C42","4B0C6B","781C6D","A52C60","CF4446","ED6925","FB9A06","F7D03C","FCFFA4"),
-    c("000004","180F3E","451077","721F81","9F2F7F","CD4071","F1605D","FD9567","FEC98D","FCFDBF"),
-    c("0D0887","47039F","7301A8","9C179E","BD3786","D8576B","ED7953","FA9E3B","FDC926","F0F921"),
-    c("00204D","00336F","39486B","575C6D","707173","8A8779","A69D75","C4B56C","E4CF5B","FFEA46") ,
-    c("AFF0E9","FFFFB3","008040","FCBA03","800000","69300D","ABABAB","FFFCFF"),
-    c("FFFFFF","B7E8FF","ACD88C","DDE241","E83535","380000"),
-    c("67001F","B2182B","D6604D","F4A582","FDDBC7","F7F7F7","D1E5F0","92C5DE","4393C3","2166AC","053061"),
-    c("40004B","762A83","9970AB","C2A5CF","E7D4E8","F7F7F7","D9F0D3","A6DBA0","5AAE61","1B7837","00441B"),
-    c("7F3B08","B35806","E08214","FDB863","FEE0B6","F7F7F7","D8DAEB","B2ABD2","8073AC","542788","2D004B"),
-    c("9E0142","D53E4F","F46D43","FDAE61","FEE08B","FFFFBF","E6F598","ABDDA4","66C2A5","3288BD","5E4FA2"),
-    c('566314','9ba864','c5cc90','a59a7f','5b4242'),
-    c('75a05b','e5d9a7','fcc575','baa395','e2e2e2'),
-    c('2b2d42','8c95ad','F4F4F4','e28f76','93021b'),
-    c('353642','9ca0ad','F4F4F4','e2b7aa','934855'),
-    c('22223b','4a4e69','9a8c98','c9ada7','f2e9e4')
+  
+  getPal <- function(name){
+    switch(which(tolower(namelist) %in% tolower(name)),
+           c("0000FF","0080FF","00FFFF","80FF80","FFFF00","FF8000","FF0000","800000"),
+           c("352A87","0567DF","108ED0","1BAEB4","6BBC85","C4BB5E","F7CB33","F9FB0E"),
+           c("440154","482878","3E4A89","31688E","26828E","1F9E89","35B779","6DCD59","B4DE2C","FDE725"),
+           c("000004","1B0C42","4B0C6B","781C6D","A52C60","CF4446","ED6925","FB9A06","F7D03C","FCFFA4"),
+           c("000004","180F3E","451077","721F81","9F2F7F","CD4071","F1605D","FD9567","FEC98D","FCFDBF"),
+           c("0D0887","47039F","7301A8","9C179E","BD3786","D8576B","ED7953","FA9E3B","FDC926","F0F921"),
+           c("00204D","00336F","39486B","575C6D","707173","8A8779","A69D75","C4B56C","E4CF5B","FFEA46") ,
+           c("AFF0E9","FFFFB3","008040","FCBA03","800000","69300D","ABABAB","FFFCFF"),
+           c("FFFFFF","B7E8FF","ACD88C","DDE241","E83535","380000"),
+           c("67001F","B2182B","D6604D","F4A582","FDDBC7","F7F7F7","D1E5F0","92C5DE","4393C3","2166AC","053061"),
+           c("40004B","762A83","9970AB","C2A5CF","E7D4E8","F7F7F7","D9F0D3","A6DBA0","5AAE61","1B7837","00441B"),
+           c("7F3B08","B35806","E08214","FDB863","FEE0B6","F7F7F7","D8DAEB","B2ABD2","8073AC","542788","2D004B"),
+           c("9E0142","D53E4F","F46D43","FDAE61","FEE08B","FFFFBF","E6F598","ABDDA4","66C2A5","3288BD","5E4FA2"),
+           c('566314','9ba864','c5cc90','a59a7f','5b4242'),
+           c('75a05b','e5d9a7','fcc575','baa395','e2e2e2'),
+           c('2b2d42','8c95ad','F4F4F4','e28f76','93021b'),
+           c('353642','9ca0ad','F4F4F4','e2b7aa','934855'),
+           c('22223b','4a4e69','9a8c98','c9ada7','f2e9e4')
+    )
+  }
+
+  
+  if (show){ # make plot of available ramps
+    windows(600,900, bg='#2d2d2d')
+    op  <- par(mar=c(0.5,6,0.5,1))
+    all <- do.call(rbind,lapply(namelist, function(x) colorRampPalette(paste0('#',getPal(x)))(number)))
+    plot(NA,axes=F,xlab='',ylab='',ylim=c(0,length(namelist)+1), xlim=c(1,number), xaxs='i', yaxs='i')
+    for (i in 1:length(namelist)){
+      for (j in 1:number){
+        irev <-  (length(namelist):1)[i]
+        polygon(x=c(j-1,j,j,j-1), y=c(irev-0.4,irev-0.4,irev+0.4,irev+0.4), border=NA, col=all[i,j])
+      }
+    }
+    mtext(namelist, side=2, line=0.5, at=length(namelist):1,padj=0.5,adj=1,las=2, cex=1.1, col='white')
+    par(op)
+    warning('No color ramp output when "show=TRUE", just a plot with available color ramps')
     
-    
-  )
-  if (reversed){colpal <- rev(colpal)}
-  outcolors <- colorRampPalette(paste0('#',colpal))(number)
-  return(outcolors)
+  }else{ # generate output
+    colpal   <- getPal(name)
+    if (reversed){colpal <- rev(colpal)}
+    outcolors <- colorRampPalette(paste0('#',colpal))(number)
+    return(outcolors)
+  }
+  
+
 }
 
