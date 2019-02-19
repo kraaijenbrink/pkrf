ed39b910e50cd7e6b03cecfaa361815c28d81fc0
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2019-02-19 11:34:35 +0100

    - renamed R scripts to reflect function names - added rmarkdown MH report template

---
+++ R/template_mh-report.r
@@ -0,0 +1,26 @@
+# mountain hydrology markdown report template
+# philip kraaijenbrink
+
+
+#' Markdown template
+#'
+#' Report for R markdoiwn with custom css and header and footers in mountainhydrology style.
+#' @export
+
+mh_report = function(...) {
+  
+  # locations of resource files in the package
+  pkg_resource = function(...) {
+    system.file(..., package = "pkrf")
+  }
+  
+  css    = pkg_resource("rmarkdown/styles/styles.css")
+  header = pkg_resource("rmarkdown/styles/mnthydro-header.html")
+  footer = pkg_resource("rmarkdown/styles/mnthydro-footer.html")
+  
+  # call the base html_document function
+  rmarkdown::html_document(...,
+                           css=css,
+                           includes = rmarkdown::includes(before_body=header, after_body=footer)
+                           )
+}
