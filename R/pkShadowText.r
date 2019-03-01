374a82ed10040eceed0cacf9345abd4e4fc51afa
Philip Kraaijenbrink <philip.kraaijenbrink@gmail.com>
2019-03-01 11:59:09 +0100

    Added function add shadowed or outlined text labels to a plot

---
+++ R/pkShadowText.r
@@ -0,0 +1,30 @@
+# Plot text labels with a different color background (taken from TeachingDemos package)
+# philip kraaijenbrink
+
+
+#' Plot shadowed text labels
+#'
+#' Plot text labels with a different color shadow or faked outline. Function taken from `TeachingDemos` package.
+#' @param x x position
+#' @param y y position
+#' @param labels text labels
+#' @param col foreground color
+#' @param bg shadow/outline color
+#' @param theta angle at which to plot the shadow
+#' @param r distance of the shadow
+#' @param ... any other arguments for text function
+#' 
+#' @return Vector with hex colors strings. 
+#' @export
+
+shadowtext <- function(x, y=NULL, labels, col='white', bg='black',
+                       theta=seq(pi/4, 2*pi, length.out=8), r=0.1, ... ) {
+  
+  xy <- xy.coords(x,y)
+  xo <- r*strwidth('A')
+  yo <- r*strheight('A')
+  
+  for (i in theta) {
+    text(xy$x + cos(i)*xo, xy$y + sin(i)*yo, labels, col=bg, ... )
+  }
+  text(xy$x, xy$y, labels, col=col, ... ) }
