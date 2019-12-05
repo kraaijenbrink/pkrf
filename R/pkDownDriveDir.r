d48dda861ea37b81df4d134408b4623295dd7318
Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2019-12-05 09:02:03 +0100

    fix the authentication method for update googledrive package

--- R/pkDownDriveDir.r
+++ R/pkDownDriveDir.r
@@ -7,20 +7,20 @@
 #' Download content of entire folder on Google Drive to a local directory
 #' @param drivePath Path to directory on Google Drive that contains the files to download.
 #' @param localPath Path where the files will be stored. Will be created if inexistent. No warnings if exists.
-#' @param authFile Path to RDS file with authentication token. If not exist, web authentication is performd and new file is created at path.
+#' @param email Google account email address.
 #' @param rmFromDrive Remove the files from Google Drive after download (logical).
 #'
 #' @return Nothing
 #' @export
-getGDriveDir <- function(drivePath, localPath, authFile, rmFromDrive=FALSE){
+getGDriveDir <- function(drivePath, localPath, email=NULL, rmFromDrive=FALSE){
 
   dp <- require(googledrive); if(dp==FALSE){stop('"googledrive" package is missing')}
+  
   # authenticate with google
-  if (file.exists(authFile)){    # use provided auth file
-    drive_auth(authFile)
-  }else{                         # use browser and generate auth file
-    token <- drive_auth(cache=F)
-    saveRDS(token,authFile)
+  if (!is.null(email)){
+    drive_auth(email)
+  }else{
+    drive_auth()
   }
   
   # get list of files in provided folder
