6f85ca18133b529028990168c37fabc0bd392227
Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2020-01-24 13:05:21 +0100

    small updates

--- R/scientific_clean.r
+++ R/scientific_clean.r
@@ -5,23 +5,14 @@
 #'
 #' Convert numbers into a scientific notation expression that uses \out{<i>2.4 x 10<sup>-3</sup></i>} format
 #' @param x Numeric vector.
-#' @param ... Any other argument passed to \code{scales::scientific()}
+#' @param ... Any other argument passed to \code{base::format()}
 #' 
 #' @return Expression vector
 #' @export
-
 scientific10 <- function(x, ...){
-  
-  # get scientific notation from scales function
-  s <- scales::scientific(x, ...)
-
-  # get separate components
-  number   <- sapply(strsplit(s,'e'), function(x) x[1])
-  expbit   <- as.integer(sapply(strsplit(s,'e'), function(x) x[2]))
-  # expsign  <- substr(expbit,0,1)
-  # exponent <- as.integer(substr(expbit,2,1e3))
-  
-  # contruct expressions
-  formatted <- parse(text=paste0(number,'%*%10^',expbit))
+  s         <- format(x,scientific=T,...)
+  number    <- sapply(strsplit(s,'e'), function(x) x[1])
+  expbit    <- as.character(as.integer(sapply(strsplit(s,'e'), function(x) x[2])))
+  formatted <- parse(text=paste0(number,'%*%10^',expbit), keep.source=T)
   return(formatted)
 }
