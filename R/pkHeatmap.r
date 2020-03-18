6b8c66b3c64ba7dcc5310a9efd993671e23d7149
Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2020-03-18 10:36:01 +0100

    add capability to pass additional axisvars

--- R/pkHeatmap.r
+++ R/pkHeatmap.r
@@ -11,19 +11,22 @@
 #' @param xlimit the x limits of the plot (vector length 2)
 #' @param ylimit the y limits of the plot (vector length 2)
 #' @param colramp the colors to use for plotting (vector)
-#' @param maketicks draw simple x,y axes at \code{pretty} tick postitions (logical).
+#' @param maketicks draw simple x,y axes (logical).
+#' @param xticks position of xticks, defaults to \code{pretty}
+#' @param yticks posiition of yticks, defaults to \code{pretty}
 #' @param makebox draw box line around plot (logical)
 #' @param axislwd linewidth to use for ticks and box (numeric)
 #' @param locut low threshold on raster values (numeric)
 #' @param hicut high threshold on raster values (numeric)
 #' @param nacut values outside cutrange are set to NA instead of cut value (logical)
+#' @param axisvars list with additional arguments to \code{axis} (named list, not including \code{side},\code{at} or \code{lwd})
 #' @param ... other parameters passed to \code{image} function
 #'
 #' @return plot
 #' @export
 heatGrid <- function(mat=NULL, xrange=NULL, yrange=NULL, xlimit=NULL, ylimit=NULL,
-                     colramp=topo.colors(200), maketicks=T, makebox=T, axislwd=0.5,
-                     locut=NULL, hicut=NULL, nacut=F,...){
+                     colramp=topo.colors(200), maketicks=T, xticks=NULL, yticks=NULL, makebox=T, axislwd=0.5,
+                     locut=NULL, hicut=NULL, nacut=F, axisvars=NULL, ...){
   
   # setup default data
   testn    <- 25
@@ -31,7 +34,6 @@ heatGrid <- function(mat=NULL, xrange=NULL, yrange=NULL, xlimit=NULL, ylimit=NUL
   testdata <- do.call(cbind,lapply(1:length(trigo), function(i) abs(rev(sort(rnorm(testn,trigo[i],0.5))))))
   # testdata <- ifelse(testdata>0.2,NA,testdata)
   if (is.null(mat)){mat <- testdata}
-  
   
   # seq axes sequencing
   if (is.null(xrange)){xrange <- c(1,ncol(mat))}
@@ -71,8 +73,18 @@ heatGrid <- function(mat=NULL, xrange=NULL, yrange=NULL, xlimit=NULL, ylimit=NUL
         ...)
   
   if (maketicks){
-    axis(1, at=pretty(xseq), lwd=axislwd)
-    axis(2, at=pretty(yseq), lwd=axislwd)
+    if (is.null(xticks)){
+      at_x <- pretty(xseq)
+    }else{
+      at_x <- xticks
+    }
+    if (is.null(yticks)){
+      at_y <- pretty(yseq)
+    }else{
+      at_y <- yticks
+    }
+    do.call(axis, c(list(side=1, at=at_x, lwd=axislwd), axisvars))
+    do.call(axis, c(list(side=2, at=at_y, lwd=axislwd), axisvars))
   }
   if (makebox){
     box(lwd=axislwd)
