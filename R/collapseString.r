c1e183b002d06fcd3999cc2d9e307271ec6bfc1b
Philip Kraaijenbrink <p.d.a.kraaijenbrink@uu.nl>
2020-11-08 16:35:56 +0100

    change input param name

--- R/collapseString.r
+++ R/collapseString.r
@@ -7,10 +7,10 @@
 #' 
 #' @param x (vector of strings
 #' @param delimiter character(s) used as delimiter
-#' @param add_quotes add quotes to each string
+#' @param quotes add quotes to each string
 #' @return collapsed string
 #' @export
-collapseString = function(x, delimiter=',',add_quotes=F) {
+collapseString = function(x, delimiter=',',quotes=F) {
   if (add_quotes){
     paste0('"',paste(x, collapse=paste0('"',delimiter,'"')),'"')
   }else{
